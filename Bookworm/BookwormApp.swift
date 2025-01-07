209f203e0b53daac36e6c878c888c70eda74ef10
Saverio Negro <saverionegro@gmail.com>
2025-01-07 19:07:27 +0100

    Initial commit.

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Saverio Negro on 07/01/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
